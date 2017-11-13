870396744fbc35187a37687108f088572381a7f1
Uli Köhler <ulikoehler@online.de>
2017-11-13 01:28:03 +0100

    More minor fixes

--- KATCUI/src/app/comment-filter.pipe.ts
+++ KATCUI/src/app/comment-filter.pipe.ts
@@ -1,7 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'commentFilter'
+    name: 'commentfilter'
 })
 export class CommentFilterPipe implements PipeTransform {
 
